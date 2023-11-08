f7ff78d3c2501c147cd4478a6f69ed582b1c8dd1
Gulnaz <gulnaz.baembitova@mail.ru>
2023-11-08 22:30:28 +0300

    last

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
 // Получаем элементы слайдера
-const img = document.querySelector('.image-1')
+const img = document.querySelector('.image')
 console.log(img);
-const prevBtn = () => {
+const nextBtn = () => {
 img.src = "assets/images/dog-tw.jpg";
-img.src = "assets/images/dog-three.jpg";
+console.log(nextBtn)
 };
-const prevBotton = document.querySelector('.prevBtn') 
 
-
-const nextBotton = document.querySelector('.nextBtn') 
-/*
-const slides = Array.from(slider.querySelectorAll('img'));
-const slideCount = slides.length;
-let slideIndex = 0;
-
-// Устанавливаем обработчики событий для кнопок
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
-
-// Функция для показа предыдущего слайда
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
+const prevBtn = () => {
+img.src = "assets/images/dog-one.jpg";
 }
 
-// Функция для показа следующего слайда
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
-}
-
-// Функция для обновления отображения слайдера
-function updateSlider() {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.style.display = 'block';
-    } else {
-      slide.style.display = 'none';
-    }
-  });
-}
-
-// Инициализация слайдера
-updateSlider();
-*/
